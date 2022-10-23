9a393a913ef6abc75edb337dba34366bc83ac043
numanakoluk <numanakoluk01@gmail.com>
2022-10-24 00:57:56 +0300

    Driver and Test Class

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,75 @@
-public class AppTest {
 
+import static org.junit.Assert.assertTrue;
+
+import driver.Driver;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import pages.GuestPage;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.ProductsPage;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest  extends Driver {
+
+    GuestPage guestPage;
+    LoginPage loginPage;
+    MainPage mainPage;
+    ProductsPage productsPage;
+
+
+    public void Init(){
+
+        guestPage=new GuestPage();
+        loginPage=new LoginPage();
+        mainPage =new MainPage();
+        productsPage=new ProductsPage();
+    }
+
+
+
+    @Test
+    @Order(1)
+    public void loginScenerio() throws InterruptedException {
+        Init();
+
+        guestPage.controlGuestPage();
+        guestPage.controlHover();
+        loginPage.controlEmailPage();
+        loginPage.loginGoogle();
+        loginPage.controlGooglePage();
+        loginPage.loginGooglePage();
+        loginPage.loginPassword();
+        loginPage.controlLoginPassword();
+        mainPage.controlMainPage();
+        mainPage.searchBoxClick();
+        mainPage.controlSelectFilter();
+        productsPage.controlSelectProducts();
+        productsPage.controlAddToCart();
+        productsPage.controlShoppingCart();
+        productsPage.otherProduct();
+        productsPage.controlShoppingDone();
+
+
+    }
+
+    @Test
+    @Order(2)
+    public void loginOutScenerio() throws InterruptedException {
+
+        Init();
+        mainPage.controlMainPage();
+        mainPage.searchBoxClick();
+        mainPage.controlSelectFilter();
+        productsPage.controlSelectProducts();
+        productsPage.controlAddToCart();
+        productsPage.controlShoppingCart();
+        productsPage.otherProduct();
+        productsPage.controlShoppingDone();
+
+
+    }
 }
