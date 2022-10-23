9cd38e265fa4e0beb5948f32422920ce4c3b47f6
numanakoluk <numanakoluk01@gmail.com>
2022-10-24 00:54:25 +0300

    Refactoring

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -4,17 +4,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Driver {
     private static final Logger logger = LogManager.getLogger(Driver.class);
