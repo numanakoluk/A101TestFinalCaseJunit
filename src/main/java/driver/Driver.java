20adbbd9d364b85769d0d9408e7b5d7a371f8536
numanakoluk <numanakoluk01@gmail.com>
2022-10-24 11:35:45 +0300

    Log4j

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import testResultMethods.TestResultLogger;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@ExtendWith(TestResultLogger.class)
 public class Driver {
     private static final Logger logger = LogManager.getLogger(Driver.class);
     public static WebDriver driver;
@@ -29,6 +36,7 @@ public class Driver {
         ChromeOptions options = new ChromeOptions();
         options.merge(desiredCapabilities);
         driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get("https://www.hepsiburada.com/");
